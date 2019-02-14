3e2a440b2ecad9935f70cc0b31525e3bd057b9c8
Luis <luis-santiago97@hotmail.com>
2019-02-14 18:36:09 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+const port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getAnio', () =>{ 
+    return new Date().getFullYear();
+});
+app.set('view engine', 'hbs');
+app.get('/',  (req, res) => {
+    
+  res.render('home', {
+      name : 'Luis',
+  });
+})
+app.get('/about',  (req, res) => {
+    
+    res.render('about', {
+    });
+  })
+ 
+app.listen(port, () => console.log(`Escuchando por el puerto ${port}`));

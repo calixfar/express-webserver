c7624db247ff5854f1c69416f3c621cc633b3b26
Luis <luis-santiago97@hotmail.com>
2019-02-21 17:49:44 -0500

    Arreglado PORT server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const hbs = require('hbs');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
